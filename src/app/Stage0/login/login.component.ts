5ec3a2434718b5d2a8be0e5f68d747e1e9f597c9
EpicPi <piyushgk1@gmail.com>
2017-10-21 01:16:15 -0500

    login works (hardcoded)

--- src/app/Stage0/login/login.component.ts
+++ src/app/Stage0/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, FormsModule, Validators} from '@angular/forms';
+import {User} from '../../Services/user';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +10,18 @@ import {FormControl, FormGroup, FormsModule, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   myForm: FormGroup;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.myForm = new FormGroup({
-      email: new FormControl(null, Validators.required),
+      userName: new FormControl(null, Validators.required),
       password: new FormControl(null, Validators.required)
     });
   }
   onSubmit() {
-    const user = new User(this.myForm.value.email, this.myForm.value.password);
-
+    const user = new User(this.myForm.value.userName, this.myForm.value.password);
+    localStorage.setItem('user', user.username);
     this.myForm.reset();
+    this.router.navigateByUrl('/landing');
   }
 }
