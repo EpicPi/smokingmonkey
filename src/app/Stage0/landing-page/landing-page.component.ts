5f15592da32ef65a05a46ccb49b022c995a694b9
EpicPi <piyushgk1@gmail.com>
2017-10-21 02:10:38 -0500

    landing page with working links

--- src/app/Stage0/landing-page/landing-page.component.ts
+++ src/app/Stage0/landing-page/landing-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Group} from '../../Services/group';
 import {UserService} from '../../Services/user.service';
 import {GroupService} from '../../Services/group.service';
 import {User} from '../../Services/user';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -12,14 +13,24 @@ import {User} from '../../Services/user';
 export class LandingPageComponent implements OnInit {
   user: User;
 
-  constructor(private userService: UserService, private groupService: GroupService) { }
+  constructor(private router: Router, private userService: UserService, private groupService: GroupService) {
+  }
 
   ngOnInit() {
     this.user = this.userService.user;
-    }
+  }
 
   selectGroup(group: Group) {
     this.groupService.group = group;
+    this.router.navigateByUrl('/group');
+  }
+
+  makeGroup() {
+   this.router.navigateByUrl('/groupCreate');
+  }
+
+  joinGroup(){
+    this.router.navigateByUrl('/groupJoin');
   }
 
 
