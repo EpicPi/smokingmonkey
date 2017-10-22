d63aac7ac8b0df488c2fc7597717431b2f419831
EpicPi <piyushgk1@gmail.com>
2017-10-22 08:56:33 -0500

    fixed landing page and group creation

--- src/app/Stage0/group-create/group-create.component.ts
+++ src/app/Stage0/group-create/group-create.component.ts
@@ -5,6 +5,7 @@ import {UserService} from '../../Services/user.service';
 import {Group} from '../../Models/group';
 import {GroupService} from '../../Services/group.service';
 import {UserStat} from '../../Models/user.stat';
+import {Assignment} from '../../Models/assignment';
 
 @Component({
   selector: 'app-group-create',
@@ -15,14 +16,15 @@ export class GroupCreateComponent implements OnInit {
   constructor(private router: Router, private userService: UserService, private groupService: GroupService){}
   defaultQuestion = 'Daily';
   name: string;
-  fre: string;
+  assesment: string;
   money: number;
+
   ngOnInit() {
   }
 
   submit() {
     console.log('doing it');
-    const group = new Group(this.name, this.money);
+    const group = new Group(this.name, this.money, new Assignment(this.assesment));
 
     group.userStats.push(new UserStat(this.userService.user, 0, group.money));
     this.groupService.addGroup(group);
