2da4b1787eecd4f5b0aaba2c0ae0a44d307a7ea5
EpicPi <piyushgk1@gmail.com>
2017-10-21 19:14:12 -0500

    firebase stuff is hard

--- src/app/Stage0/landing-page/landing-page.component.ts
+++ src/app/Stage0/landing-page/landing-page.component.ts
@@ -4,33 +4,46 @@ import {UserService} from '../../Services/user.service';
 import {GroupService} from '../../Services/group.service';
 import {User} from '../../Models/user';
 import {Router} from '@angular/router';
+import {FirebaseService} from '../../Services/firebase.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent implements OnInit{
+export class LandingPageComponent implements OnInit {
   user: User;
+  groups: Group[];
 
-  constructor(private router: Router, private userService: UserService, private groupService: GroupService) {
+  constructor(private router: Router, private userService: UserService, private groupService: GroupService, private fb: FirebaseService) {
   }
+
   // constructor(private userService: UserService, private router: Router){}
 
   ngOnInit() {
     this.user = this.userService.user;
+    this.fb.getGroups().subscribe(res => {
+      const groups = this.groupService.getGroups(res);
+      for (let group of groups) {
+      }
+      for (let us of group)
+        this.groups.push(group);
+    });
+    this.groups = this.groupService.getGroupsbyUser(this.user);
   }
 
   selectGroup(group: Group) {
     this.groupService.group = group;
+    this.fb.addGroup(group).subscribe();
+    this.fb.getGroups().subscribe(console.log);
     this.router.navigateByUrl('/group');
   }
 
   makeGroup() {
-   this.router.navigateByUrl('/groupCreate');
+    this.router.navigateByUrl('/groupCreate');
   }
 
-  joinGroup(){
+  joinGroup() {
     this.router.navigateByUrl('/groupJoin');
   }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {UserService} from './Services/user.service';
+import {FirebaseService} from './Services/firebase.service';
+import {GroupService} from './Services/group.service';
+import {Group} from './Models/group';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +11,7 @@ import {UserService} from './Services/user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router, private userService: UserService) {
+  constructor(private router: Router, private userService: UserService, private fb: FirebaseService, private groupService: GroupService) {
   }
 
   ngOnInit(): void {
