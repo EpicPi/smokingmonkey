a25ddbb26379f0d06f5a47d7cf8ca31e5bce2c4e
EpicPi <piyushgk1@gmail.com>
2017-10-22 04:06:32 -0500

    nav-bar has been extracted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,6 +52,7 @@ import {
   MatTooltipModule,
   MatStepperModule,
 } from '@angular/material';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -65,6 +66,7 @@ import {
     InfoComponent,
     GroupEndComponent,
     GroupJoinComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentChecked, Component, OnInit} from '@angular/core';
+import {UserService} from '../Services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements AfterContentChecked {
+  userName: string;
 
-  constructor() { }
+  constructor(private userService: UserService) {
+  }
 
-  ngOnInit() {
+  ngAfterContentChecked() {
+    if (!this.userName && this.userService.user) {
+      this.userName = this.userService.user.username;
+    }
   }
 
 }
